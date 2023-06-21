c8f8c18db7a8885e5b688c254de0096f8a3ac7dd
Sharma <Sharmaisse@gmail.com>
2023-06-21 11:21:50 -0700

    50 Fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Lists all the natural numbers below 1024 (excluded)
+ * main - Lists Write a program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line.all the natural numbers below 1024 (excluded)
  *        that are multiples of 3 or 5.
  *
  * Return: Always 0.
@@ -9,6 +9,35 @@
 int main(void)
 {
 	int i, sum = 0;
+include <stdio.h>
+
+/**
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *        separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
 
 	for (i = 0; i < 1024; i++)
 	{
