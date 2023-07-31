c43e50f3518304afa08d5bc7ca2b3b61648c6b41
Sharma <Sharmaisse@gmail.com>
2023-07-31 12:08:46 -0700

    Adding nodes at the end and returning if it fails

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include <string.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_node_end - Adding a new node at the end
+ * @str: Adding a string 
+ * @head: Doubeling the pointers in the list
+ *
+ * Return : Null or address
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *temp = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new
+		return (new);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
+}
