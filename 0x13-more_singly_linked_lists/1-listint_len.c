1b77f7b78049972e991abd025e1bd15af4b94d91
Sharma <Sharmaisse@gmail.com>
2023-08-01 11:00:08 -0700

    2functionality that reurns no. of elements in a linked list Task.1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/* struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * @h: linked list of type listint_t
+ *  Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
