f7904d6165f0f3a80ceb1519cac097f9e7a1cf94
Sharma <Sharmaisse@gmail.com>
2023-08-01 10:49:28 -0700

    Task0-Print list Function that printrs all elements of list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of alistint_t linked list
+ * @h: linked list of listint_t to print
+ *
+ * Return:The number of nodes
+ */
+	size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
