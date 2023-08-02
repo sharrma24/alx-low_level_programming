a1eecab2b4938930e91900df82f0e489198e7660
Sharma <Sharmaisse@gmail.com>
2023-08-02 14:26:44 -0700

    TASK0 molloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- * if malloc fails, the malloc_checked function should cause normal process termination
+ * malloc_checked- the malloc_checked function should cause normal process termination
  * @b: bytes to be allocated
  *
- * Return: printiong a pointer into the assigned memory
+ * Return: a pointer into the assigned memory
  */
 void *malloc_checked(unsigned int b)
 {
