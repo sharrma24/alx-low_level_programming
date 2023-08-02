66f8da35437a8c0a2c02469957fc591e7954a37e
Sharma <Sharmaisse@gmail.com>
2023-08-02 12:30:45 -0700

    TASK4 Singly list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,15 +5,16 @@
  * free_list - free a linked_list
  * @head: list_t list frees when printed
  */
+
 void free_list(list_t *head)
 {
-	list_t *temp;
+ list_t *temp;
 
-	while (head)
-	{
-		temp = head->next;
-		free(head->str);
-		free(head);
-		head = temp;
-	}
+ while (head)
+ {
+ temp = head->next;
+ free(head->str);
+ free(head);
+ head = temp;
+ }
 }
