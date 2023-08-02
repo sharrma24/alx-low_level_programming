cf48332d3346a4204ca7b8b1ecdd07e8460fb1a0
Sharma <Sharmaisse@gmail.com>
2023-08-02 14:19:35 -0700

    TASK2 nodes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
-#include "lists.h"
 #include <string.h>
+#include "lists.h"
+
 /**
  *  @str: addyin  new strings to the node
  *   add_node- in the linked_list it adds a new node at the begginning
@@ -19,9 +20,11 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
 	new->next = (*head);
 	(*head) = new;
-        new->str = strdup(str);
-	new->len = len;
+
 	return (*head);
 }	
