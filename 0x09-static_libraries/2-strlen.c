aaedee4a43b0e24f62104d4511bca2fc9475c445
Sharma <sharmaisse@gmail.com>
2023-09-01 12:40:14 -0700

    isupper.c memset.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @s: strings
+ * Return: Length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+
+	return (longi);
+}
