c514f7674b124bb10cf0f9225b822e9198cf83ac
Sharma <sharmaisse@gmail.com>
2023-10-25 10:46:19 -0700

    PUTS TASK0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,17 @@
 #include "main.h"
-
 /**
-  * _puts_recursion - main function
-  *
-  * @s:  parameter
-  *
-  * Return: return 0.
-  */
+ * _puts_recursion - function  puts();
+ * @s: input v
+ * Return: Always 0 (Success)
+ */
 void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-	_putchar(*s);
-	_puts_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+
 	else
-	_putchar('\n');
+		_putchar('\n');
 }
