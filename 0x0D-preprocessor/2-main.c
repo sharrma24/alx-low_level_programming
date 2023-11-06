49897ba90311665e4153c148e1b3f81281ceba97
Sharma <sharmaisse@gmail.com>
2023-11-06 10:10:04 -0800

    main.h files:

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print name of the file
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,3 @@
+All projects
+Processor
+Readme
