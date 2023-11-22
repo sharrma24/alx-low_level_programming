06f37599b536a59df42163d40a55683ff053d2a3
Sharma <sharmaisse@gmail.com>
2023-11-22 11:15:31 -0800

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,10 +4,10 @@ int check_pal(char *s, int i, int len);
 int _strlen_recursion(char *s);
 
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s:  reverse the string
+ * is_palindrome - checks if a string is  palindrome
+ * @s:  revers
  *
- * Return: 1 if it is, 0 it's not
+ * Return: return
  */
 int is_palindrome(char *s)
 {
@@ -16,12 +16,6 @@ int is_palindrome(char *s)
 	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
-/**
- * _strlen_recursion - returns the length of a string
- * @s: calculate the length 
- *
- * Return: length of the string
- */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
